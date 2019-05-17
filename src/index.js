34adf7d7563d47693c01af811a542ce97601e637
Onengiye Richard <klevamane@gmail.com>
2019-05-17 18:28:12 +0100

    add router redirect to home if user is already logged in

--- src/index.js
+++ src/index.js
@@ -2,24 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 
 // Add semantic ui css
 import 'semantic-ui-css/semantic.min.css';
 
-const Root = () => (
-    <Router>
+class Root extends React.Component {
+    componentDidMount() {
+        // detects whether we have a user in our app
+        firebase.auth().onAuthStateChanged(user => {
+            if(user) {
+                this.props.history.push('/');  
+            }
+        })
+    }
+    render() { 
+        return(
         <switch>
             {/* exact keyword ensures that the right route is matched */}
             <Route exact path="/" component={App} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
         </switch>
-    </Router>
-);
+        );
+    }
+}
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+<Router>
+<RootWithAuth />
+</Router>, 
+document.getElementById('root'));
 registerServiceWorker();
