62196668a365a71cf2cd561fd83201f1231ae4b1
Onengiye Richard <klevamane@gmail.com>
2019-05-17 21:10:52 +0100

    ft(components) add more components
    - add metaPanel component
    - add sidePanel component
    - add Messages component
    - add colourPanel component

---
+++ src/components/ColourPanel/ColourPanel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ColourPanel extends Component {
+    
+    render() {
+        return (
+            <div>ColourPanel</div>
+        )
+    }
+}
+
+export default ColourPanel;
---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Messages from '../Messages/Messages';
+
+class SidePanel extends Component {
+    
+    render() {
+        return (
+            <div>SidePanel</div>
+        )
+    }
+}
+
+export default SidePanel;
