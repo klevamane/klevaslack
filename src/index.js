b800a8f8b1fcea346982406885a3725d237281b1
Onengiye Richard <klevamane@gmail.com>
2019-05-17 19:20:10 +0100

    feat(add reducer, actions) Add root reducer, actions and types
    - add the root reducer
    - add the setuser action
    - update index.js
    - implement reroute to '/' if user is authenticated and prevent from accessing the auth routes
    - add provider

--- src/index.js
+++ src/index.js
@@ -3,19 +3,33 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from 'firebase';
-
+import { connect } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 
 // Add semantic ui css
 import 'semantic-ui-css/semantic.min.css';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+// Actions
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
     componentDidMount() {
         // detects whether we have a user in our app
         firebase.auth().onAuthStateChanged(user => {
             if(user) {
+                // the connect, and mapDispatchToProps takes the 
+                // setUser action and put it on the props object of the component that is being wrapped with connect
+                this.props.setUser(user)
                 this.props.history.push('/');  
             }
         })
@@ -32,11 +46,14 @@ class Root extends React.Component {
     }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-<Router>
-<RootWithAuth />
-</Router>, 
-document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
